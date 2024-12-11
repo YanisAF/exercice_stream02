8585f74183b567ab530d4f75b9305f2ec86e3fbd
unknown <yanissf2@gmail.com>
2024-12-11 14:02:52 +0100

    add exercice stream02

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import org.example.model.Commande;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Liste de commandes
+        List<Commande> commandes = Arrays.asList(
+                new Commande(1, "Alice", Arrays.asList("Ordinateur", "Souris"), 1200.50, true),
+                new Commande(2, "Bob", Arrays.asList("Clavier", "Écran"), 300.75, false),
+                new Commande(3, "Charlie", Arrays.asList("Imprimante"), 150.00, true),
+                new Commande(4, "Alice", Arrays.asList("USB", "Casque"), 75.50, false),
+                new Commande(5, "Bob", Arrays.asList("Tablette"), 200.00, true)
+        );
+
+        commandes.stream()
+                .forEach(commande -> System.out.println(commande));
+
+        System.out.println("\nTâche 2\n");
+        commandes.stream()
+                .map(Commande::getClient).forEach(System.out::println);
+
+        System.out.println("\nTâche 3\n");
+        double result = commandes.stream()
+                .mapToDouble(Commande::getMontantTotal)
+                .sum();
+        System.out.println(result);
+
+        System.out.println("\n Tâche 4 \n");
+        commandes.stream()
+                .forEach(fields -> System.out.println("Articles : "+fields.getArticles()+", Montant total : "+fields.getMontantTotal()));
+
+        System.out.println("\n Tâche 5 \n");
+        commandes.stream()
+                .filter(livre -> livre.isEstLivree())
+                .forEach(System.out::println);
+
+        System.out.println("\n Tâche 6 \n");
+        Map<String, Double> resultat = commandes.stream()
+                .collect(Collectors.groupingBy(Commande::getClient, Collectors.summingDouble(Commande::getMontantTotal)));
+        System.out.println(resultat);
+
+
+
+
+    }
+}
---
+++ src/main/java/org/example/model/Commande.java
@@ -0,0 +1,68 @@
+package org.example.model;
+
+import java.util.List;
+
+public class Commande {
+    private int id;
+    private String client;
+    private List<String> articles;
+    private double montantTotal;
+    private boolean estLivree;
+
+    public Commande(int id, String client, List<String> articles, double montantTotal, boolean estLivree) {
+        this.id = id;
+        this.client = client;
+        this.articles = articles;
+        this.montantTotal = montantTotal;
+        this.estLivree = estLivree;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+
+    public List<String> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<String> articles) {
+        this.articles = articles;
+    }
+
+    public double getMontantTotal() {
+        return montantTotal;
+    }
+
+    public void setMontantTotal(double montantTotal) {
+        this.montantTotal = montantTotal;
+    }
+
+    public boolean isEstLivree() {
+        return estLivree;
+    }
+
+    public void setEstLivree(boolean estLivree) {
+        this.estLivree = estLivree;
+    }
+
+     @Override
+    public String toString(){
+        return  "Id client : "+id+
+                ", Nom : "+client+
+                ", Articles : "+articles+
+                ", Montant total : "+montantTotal+
+                ", Statut de la livraison : "+estLivree;
+     }
+}
